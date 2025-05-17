3e3ad714f5ef878aa0ed16fcae51103fcb4af7a6
Juan  David Cruz <114003990+JuanDa72@users.noreply.github.com>
2025-05-17 13:18:06 -0500

    Update metodoSecantes.m

--- metodoSecantes.m
+++ metodoSecantes.m
@@ -1,15 +1,28 @@
-function [aproximacion,iteraciones]=mSecantes(f,a,b,error)
-    iteraciones=0
-    while abs(b-a)>error
-        n = b - (f(b) * (b - a)) / (f(b) - f(a));
-        a=b
-        b=n
-        iteraciones=iteraciones+1
+function [aproximacion, iteraciones, resultados] = mSecantes(f, a, b, error)
+    iteraciones = 0;
+    resultados = [];  % Inicializa la matriz de resultados
+    while abs(f(b)) > error
+        difference = a - b;
+        fa = f(a);
+        fb = f(b);
+        differenceF = fa - fb;
+        n = b - (fb * (difference / differenceF));
+        
+        % Guarda los resultados de cada iteración
+        iteraciones = iteraciones + 1;
+        resultados = [resultados; iteraciones, a, b, f(a), f(b), n];
+        
+        a = b;
+        b = n;
     end
-    aproximacion=b
+    aproximacion = b;
 end
 
-f=@(x) exp(1)^(-x)-x
-[raiz,iteraciones]=mSecantes(f,0,1,10^(-1))
+f = @(x) x^3 - 6*x^2 + 11*x - 6;
+[raiz, iteraciones, resultados] = mSecantes(f, 1.5, 2.8, 10^(-3));
+
+% Imprimir la matriz de resultados
+disp('Iteración     a         b         f(a)       f(b)         n');
+disp(resultados);
 disp(['Raíz: ', num2str(raiz)]);
 disp(['Iteraciones: ', num2str(iteraciones)]);
